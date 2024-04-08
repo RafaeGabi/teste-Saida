06a45403b4edf34a3c6cdfcf7c19c16705a30d97
Rafael Rodrigues <rafaprodrigues1996@gmail.com>
2024-04-08 17:32:11 -0300

    teste-Saida

---
+++ src/testesaida.java
@@ -0,0 +1,9 @@
+public class testesaida {
+    public static void main(String[] args) {
+        System.out.print("Bem vindo ao");
+        System.out.println(" mundo Java");
+        System.out.print("esta eh a ultima linha de ");
+        System.out.println("texto mostrada pelo programa");
+    }
+
+}
